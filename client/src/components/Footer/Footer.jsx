65abe0994095032edfa6c43739ae165bf1e97e8f
Thai Gia Lac <lacthai08@gmail.com>
2022-03-26 21:16:15 +0700

    lala

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,46 +1,46 @@
+
 import React from 'react';
-import {FaFacebook,FaInstagram,
-    FaTiktok,FaMarker,FaHeart} from "react-icons/fa";
-import "./footer.css";
-import myImage from "./logo1.png";
 
-
+import "./footer.css"
+import IMG from "./logo1.png"
 function Footer() {
-    const style = { marginRight: "10px" };
+   
     return (
-        <div className="App">
-            <div className="footer-container">
-                <div className="row">
-                    <div className="col1">
-                        <img className="img-footer" src={myImage} alt="" />
-                        <button className="button-footer"><FaHeart className="icon-H" style={style} />Cooperate With Us</button>
-                    </div>
-                    <div className="col2">
-                        <h2>ABOUT US</h2>
-                        <ul className="list-footer-col2">
-                            <li>Information</li>
-                            <li>Troubleshooting</li>
-                            <li>Contact - suggestions</li>
-                            <li>Service</li>
-                        </ul>
-                    </div>
-                    <div className="col3">
-                        <h2>Follow us on</h2>
-                        <ul className="list-footer-col2">
-                            <li><FaFacebook className="icon-F" style={style} />Facebook</li>
-                            <li><FaInstagram className="icon-I" style={style} />Instagram</li>
-                            <li><FaTiktok className="icon-T" style={style}  /><FaTiktok className="icon-T1" />Tiktok</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="line-footer"></div>
-                <div className="text-last-footer">
-                    <h2><FaMarker style={style} />MAKE BY <span>LALATEAM</span></h2>
-                </div>
-                
-            </div>
+        
+<div className="footer-container">
+    <div className="row">
+        <div className="col1">
+            <img className="img-footer" src={IMG} alt="" />
+            <button className="button-footer"><i className="icon-H fa-solid fa-heart icon"></i>Cooperate With Us</button>
         </div>
+        <div className="col2">
+            <h2>ABOUT US</h2>
+            <ul className="list-footer-col2">
+                <li>Information</li>
+                <li>Troubleshooting</li>
+                <li>Contact - suggestions</li>
+                <li>Service</li>
+            </ul>
+        </div>
+        <div className="col3">
+            <h2>Follow us on</h2>
+            <ul className="list-footer-col2">
+                <li><i className="icon-F fa-brands fa-facebook icon" ></i>Facebook</li>
+                <li><i className="icon-I fa-brands fa-instagram icon" ></i>Instagram</li>
+                <li><i className="icon-T fa-brands fa-tiktok icon"> </i><i className="icon-T1 fa-brands fa-tiktok"></i>Tiktok</li>
+            </ul>
+        </div>
+    </div>
+    <div className="line-footer"></div>
+    <div className="text-last-footer">
+        <h2><i className="fa-solid fa-marker icon" ></i>MAKE BY <span>LALATEAM</span></h2>
+    </div>
+
+</div>
+ 
     );
 }
 
 export default Footer;
+
+
--- client/src/pages/Cart1/Cart1.jsx
+++ client/src/pages/Cart1/Cart1.jsx
@@ -3,14 +3,13 @@ import React, { useState } from 'react'
 
 import "./cart.css"
 
-import Footer from '../../components/Footer/Footer';
 
 function Cart1() {
   
     return (
        
         <div>
-            <Footer/>
+         
         </div>
     )
 }
