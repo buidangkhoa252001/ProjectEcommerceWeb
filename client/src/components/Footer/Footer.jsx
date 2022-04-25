9a78499a10433c34d109698c32e422f4d8b12b8a
Thai Gia Lac <lacthai08@gmail.com>
2022-04-25 15:47:50 +0700

    change home page 25/4

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,43 +2,43 @@ import React from 'react';
 import "./footer.css"
 import IMG from "./logo1.png"
 function Footer() {
-   
+
     return (
-        
-    <div className="footer-container">
-        <div className="row">
 
-        
-        <div className="col1"> 
-            <img className="img-footer" src={IMG} alt="" />
-            <button className="button-footer"><i className="icon-H fa-solid fa-heart icon" ></i>Cooperate With Us</button>
+        <div className="footer-container">
+            <div className="footer-container_detail">
+                <div className="row">
+                    <div className="col1">
+                        <div className="col1_image">
+                            <img className="img-footer" src={IMG} alt="" />
+                        </div>
+                        <button className="button-footer"><i className="icon-H fa-solid fa-heart icon" ></i>Cooperate With Us</button>
+                    </div>
+                    <div className="col2">
+                        <h2>ABOUT US</h2>
+                        <ul className="list-footer-col2">
+                            <li>Information</li>
+                            <li>Troubleshooting</li>
+                            <li>Contact - suggestions</li>
+                            <li>Service</li>
+                        </ul>
+                    </div>
+                    <div className="col3">
+                        <h2>Follow us on</h2>
+                        <ul className="list-footer-col2">
+                            <li><i className="icon-F fa-brands fa-facebook icon" ></i>Facebook</li>
+                            <li><i className="icon-I fa-brands fa-instagram icon" ></i>Instagram</li>
+                            <li><i className="icon-T fa-brands fa-tiktok icon"> </i><i className="icon-T1 fa-brands fa-tiktok"></i>Tiktok</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="line-footer"></div>
+                <div className="text-last-footer">
+                    <h2><i className="fa-solid fa-marker icon" ></i>MAKE BY <span>LALATEAM</span></h2>
+                </div>
+            </div>
+        </div>
 
-        </div>
-        <div className="col2">
-            <h2>ABOUT US</h2>
-            <ul className="list-footer-col2">
-                <li>Information</li>
-                <li>Troubleshooting</li>
-                <li>Contact - suggestions</li>
-                <li>Service</li>
-            </ul>
-        </div>
-        <div className="col3">
-            <h2>Follow us on</h2>
-            <ul className="list-footer-col2">
-                <li><i className="icon-F fa-brands fa-facebook icon" ></i>Facebook</li>
-                <li><i className="icon-I fa-brands fa-instagram icon" ></i>Instagram</li>
-                <li><i className="icon-T fa-brands fa-tiktok icon"> </i><i className="icon-T1 fa-brands fa-tiktok"></i>Tiktok</li>
-            </ul>
-        </div>
-    </div>
-    <div className="line-footer"></div>
-    <div className="text-last-footer">
-        <h2><i className="fa-solid fa-marker icon" ></i>MAKE BY <span>LALATEAM</span></h2>
-    </div>
-
-</div>
- 
     );
 }
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
- 
-const Home = () => {
+import "./home.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import laptop1 from './laptop_1.png';
+import laptop2 from './laptop_2.png';
+import laptop3 from './laptop_3.png';
+import laptop4 from './laptop_4.png';
+import laptop5 from './laptop_5.png';
+import laptop6 from './laptop_6.png';
+import laptop7 from './laptop_7.png';
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
     return (
-        <div>
-            Homeq
-        </div>
+      <div
+        className={className}
+        style={{ ...style, display: "block"}}
+        onClick={onClick}
+      />
     );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", zIndex: "2", }}
+        onClick={onClick}
+      />
+    );
+  }
+
+class Home extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />
+        };
+        return (
+            <div className="slider">
+                <Slider {...settings}>
+                    <div className="image-slider">
+                        <div className="image-item">
+                            <img src={laptop1} alt="image_2" />
+                        </div>
+                    </div>
+                    <div className="image-slider">
+                        <div className="image-item">
+                            <img src={laptop2} alt="image_2" />
+                        </div>
+                    </div>
+                    <div className="image-slider">
+                        <div className="image-item">
+                            <img src={laptop3} alt="image_3" />
+                        </div>
+                    </div>
+                    <div className="image-slider">
+                        <div className="image-item">
+                            <img src={laptop4} alt="image_4" />
+                        </div>
+                    </div>
+                </Slider>
+                <div className="img_grid">
+                    <div className="img_grid-item">
+                        <img src={laptop5} alt="image_5" />
+                    </div>
+                    <div className="img_grid-item">
+                        <img src={laptop6} alt="image_6" />
+                    </div>
+                    <div className="img_grid-item">
+                        <img src={laptop7} alt="image_4" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
- 
 export default Home;
